62b14de60dbecf191c326dfebbab0b673f4e8948
unknown <tojiikan@gmail.com>
2021-11-12 08:06:04 +0100

    barreScore & Selector

--- js.js
+++ js.js
@@ -2,6 +2,18 @@ let statutCarre1 = false ;
 let statutCarre2 = false ;
 let statutCarre3 = false ;
 let statutCarre4 = false ;
+let randomPassword ;
+let randomLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+"p","q","r","s","t","u","v","w","x","y","z"];
+let randomUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
+        "P","Q","R","S","T","U","V","W","X","Y","Z"];
+let randomNumber = ["0","1","2","3","4","5","6","7","8","9"];
+let randomSpecialCharacters = ["~", "}","|","{","'","-","^","]","\\","[","@","?",">","=","<",";",
+":","/",".","_",",","+","*",")","(","`","&","%","$","#",'"',"!"," "];
+console.log("lower case : " + randomLowerCase.length);
+console.log("upper case : " + randomUpperCase.length);
+console.log("number : " + randomNumber.length);
+console.log("special character : " + randomLowerCase.length);
 
 document.body.addEventListener("click",(e) => {
     console.log(e.target.id)
@@ -11,6 +23,7 @@ document.body.addEventListener("click",(e) => {
         console.log("ok")
         document.getElementById("carre1").style.transform = "scale(0.7)";
         document.getElementById("carre1").style.backgroundColor = "var(--selector)";
+        
         } else if(statutCarre1 === true) {
             statutCarre1 = false;
             document.getElementById("carre1").style.transform = "scale(1)";
@@ -52,3 +65,34 @@ document.body.addEventListener("click",(e) => {
     }
     
 })
+
+let score = document.getElementById("scoreSelector").innerHTML;
+console.log("score : " + score)
+document.getElementById("generateBtn").addEventListener("click", () => {
+    if (statutCarre1 === true) {
+        for(let i = 0 ; i <= score;i++){
+            randomPassword = random() * randomLowerCase.length;
+
+        }
+    }
+})
+
+document.getElementById("selector").addEventListener("click", (e) => {
+    console.log("barre : " + e.clientX)
+    if (e.clientX >= 386 && e.clientX <= 527){
+    document.getElementById("selectorBar").style.transform = `translateY(-5px) translateX(${e.clientX - 376}px)`
+    } else if (e.clientX <= 386) {
+        document.getElementById("selectorBar").style.transform = `translateY(-5px) translateX(10px)`
+    } else {
+        document.getElementById("selectorBar").style.transform = `translateY(-5px) translateX(151px)`
+    }
+    let score = 4 + Math.floor((e.clientX - 386 ) / 7.05);
+    if (score <= 4) {
+        score = 4;
+    } else if(score >= 24) {
+        score = 24;
+    }
+    document.getElementById("scoreSelector").innerHTML = `${score}`
+    }
+    
+)
